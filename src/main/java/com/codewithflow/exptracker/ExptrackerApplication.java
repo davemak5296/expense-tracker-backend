746142a29277cc6650f160943566d2f331e4736d
Dave Mak <mak9652615@gmail.com>
2024-02-26 14:06:34 +0800

    git init

---
+++ src/main/java/com/codewithflow/exptracker/ExptrackerApplication.java
@@ -0,0 +1,14 @@
+package com.codewithflow.exptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class ExptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExptrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithflow/exptracker/ExptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithflow.exptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
