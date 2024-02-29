af057c65378a7abab61891dead5d6a486a85b341
Dave Mak <mak9652615@gmail.com>
2024-02-29 12:46:33 +0800

    add User Entity

---
+++ src/main/java/com/codewithflow/exptracker/entity/User.java
@@ -0,0 +1,64 @@
+package com.codewithflow.exptracker.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.validator.constraints.Length;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity implements Serializable {
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Length(min = 1, max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
+    private String alias;
+
+    @NotNull
+    @ColumnDefault("false")
+    @Column(name = "is_block")
+    private Boolean isBlock;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public Boolean getBlock() {
+        return isBlock;
+    }
+
+    public void setBlock(Boolean block) {
+        isBlock = block;
+    }
+}
