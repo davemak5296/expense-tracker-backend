cf6586d98e40430504e133bc5b09a75739263bd0
Dave Mak <mak9652615@gmail.com>
2024-02-29 13:31:27 +0800

    add UserControllerTest

---
+++ src/test/java/com/codewithflow/exptracker/controller/UserControllerTest.java
@@ -0,0 +1,48 @@
+package com.codewithflow.exptracker.controller;
+
+import com.codewithflow.exptracker.AbstractTestContainer;
+import com.codewithflow.exptracker.dto.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserControllerTest extends AbstractTestContainer {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<UserDTO> jsonUserDTO;
+
+    @Test
+    public void canRetrieveByIdWhenExists() throws Exception {
+
+        MockHttpServletResponse response = mvc.perform(
+                get("/user/1")
+                        .accept("application/json"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        assertThat(response.getContentAsString()).isEqualTo(
+                jsonUserDTO.write(new UserDTO("zzz@gmail.com", "test1234", "zzz")).getJson()
+        );
+    }
+}
