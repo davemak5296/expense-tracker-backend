9ec374c6e86cfa7cfb3c945e7b44d0c0c1453c9d
Dave Mak <mak9652615@gmail.com>
2024-02-29 12:46:06 +0800

    add BaseEntity; add @EnableTransactionManagement and @EnableJpaAuditing on app class

--- src/main/java/com/codewithflow/exptracker/ExptrackerApplication.java
+++ src/main/java/com/codewithflow/exptracker/ExptrackerApplication.java
@@ -2,9 +2,13 @@ package com.codewithflow.exptracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaAuditing
 public class ExptrackerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/codewithflow/exptracker/entity/BaseEntity.java
@@ -0,0 +1,50 @@
+package com.codewithflow.exptracker.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false, name = "created_date")
+    protected LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    protected LocalDateTime lastModifiedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
