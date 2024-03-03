eed0d0606dd98d622a4e1c516f7a340c6382d8fc
Dave Mak <mak9652615@gmail.com>
2024-03-03 16:44:59 +0800

    add MvcConfiguration to enable String response conversion

---
+++ src/main/java/com/codewithflow/exptracker/springconfig/MvcConfiguration.java
@@ -0,0 +1,19 @@
+package com.codewithflow.exptracker.springconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        WebMvcConfigurer.super.configureMessageConverters(converters);
+        converters.add(0, new MappingJackson2HttpMessageConverter());
+    }
+}
