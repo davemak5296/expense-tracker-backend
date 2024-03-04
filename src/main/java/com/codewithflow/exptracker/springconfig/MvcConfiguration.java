e35e9203acc9096a9a23d856b2691730e9fd3902
Dave Mak <mak9652615@gmail.com>
2024-03-04 21:52:43 +0800

    refactor: configure dateformat of MessageConverter

--- src/main/java/com/codewithflow/exptracker/springconfig/MvcConfiguration.java
+++ src/main/java/com/codewithflow/exptracker/springconfig/MvcConfiguration.java
@@ -2,9 +2,11 @@ package com.codewithflow.exptracker.springconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Configuration
@@ -14,6 +16,10 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 
         WebMvcConfigurer.super.configureMessageConverters(converters);
-        converters.add(0, new MappingJackson2HttpMessageConverter());
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+                .indentOutput(true)
+                .dateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+
+        converters.add(0, new MappingJackson2HttpMessageConverter(builder.build()));
     }
 }
