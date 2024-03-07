601f751481b5ed9cceb29dbdb875b90280459fe8
Dave Mak <mak9652615@gmail.com>
2024-03-07 14:54:26 +0800

    add specification-arg-resolver library to handle dynamic database filtering with http parameters (req param, path var, req body, header etc.)

--- src/main/java/com/codewithflow/exptracker/springconfig/MvcConfiguration.java
+++ src/main/java/com/codewithflow/exptracker/springconfig/MvcConfiguration.java
@@ -1,9 +1,11 @@
 package com.codewithflow.exptracker.springconfig;
 
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.text.SimpleDateFormat;
@@ -18,8 +20,13 @@ public class MvcConfiguration implements WebMvcConfigurer {
         WebMvcConfigurer.super.configureMessageConverters(converters);
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
                 .indentOutput(true)
-                .dateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+                .dateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
 
         converters.add(0, new MappingJackson2HttpMessageConverter(builder.build()));
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
     }
 }
